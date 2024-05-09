b5f0cfc7c7ccabad305a0239c8fe8e705250265f
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2024-05-09 17:10:08 +0800

    Add files via upload

--- Test-Launch-Aauto.py
+++ Test-Launch-Aauto.py
@@ -7,33 +7,14 @@ def lambda_handler(event, context):
     ssm = boto3.client('ssm')
     ec2 = boto3.client('ec2')
 
-    # Extract bucket name and file key from the S3 event    
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    ### Initial processing   
     file_key = event['Records'][0]['s3']['object']['key']
-    interface_file_key = 'HostToIpa/interface_file_hosttoipa.json'
-        
-    # Retrieve interface file from S3    
-    try:
-        response = s3.get_object(Bucket=bucket_name, Key=interface_file_key)
-        interface_file_contents = response['Body'].read().decode('utf-8')
-        try:
-            process_name_mapping = json.loads(interface_file_contents)
-        except json.JSONDecodeError:
-            print("[ERR]: Unable to parse the interface file as JSON.")
-            return
-    except Exception as e:
-        print(f"[ERR]: An error occurred while retrieving the interface file from S3. {str(e)}")
-        return
-    
-    # Get the uploaded file name and process name    
-    uploaded_file_name = file_key.split('/')[-1]
-    process_name = process_name_mapping.get(uploaded_file_name)
-    
-    if not process_name:
-        print(f"[INFO]: No process name found for file {uploaded_file_name}.")
-        return
-    
-    # Get instance information
+    file_name = os.path.basename(file_key)
+    table_id = os.path.splitext(file_name)[0]
+
+    print(f"[INFO]：Start A-AUTO job startup process. Target table ID：[{table_id}]")
+
+    ### Get instance information
     try:
         name_tag_value = os.environ['INSTANCE_NAME_TAG']
         
@@ -45,20 +26,39 @@ def lambda_handler(event, context):
         instance_id = instance_details['Reservations'][0]['Instances'][0]['InstanceId']
     except Exception as e:
         print("[ERR]: Could not obtain instance information.")
-        print("[INFO]: A-AUTO job startup processing terminated abnormally.")
-        return {'success': False}
+        print("[INFO]: A-AUTO job startup processing terminated abnormally. Target table ID：[{table_id}]")
+        return
 
-    # A-AUTO job execution command sending process    
+    ### Set values from S3 Lambda configuration file
     try:
-        command = f'cip s{process_name}'
+        bucket_name = os.environ['S3_BUCKET_NAME']
+        json_file_base_path = os.environ['JSON_FILE_PATH']
+        json_file_path = os.path.join(json_file_base_path, 'interface_file_hosttoipa.json')
+        s3_response = s3.get_object(Bucket=bucket_name, Key=json_file_path)
+        json_data = json.loads(s3_response['Body'].read().decode('utf-8'))
+        network_id = json_data[table_id]
+    except Exception as e:
+        print(f"[ERR]：Could not obtain value from Lambda configuration file. Target table ID：[{table_id}]")
+        print(f"[INFO]：A-AUTO job startup processing terminated abnormally. Target table ID：[{table_id}]")
+        return
+    
+    ### A-AUTO job execution command sending process    
+    try:
+        command = f'cip s{network_id}'
+        document_name = os.environ['DOCUMENT_NAME']
         cloudwatch_log_group = '/aws/lambda/Test-Launch-Aauto'
 
         response = ssm.send_command(
             InstanceIds=[instance_id],
-            DocumentName='AWS-RunPowerShellScript',
+            DocumentName = document_name,
             Parameters={'commands': [command]},
             CloudWatchOutputConfig={'CloudWatchOutputEnabled': True, 'CloudWatchLogGroupName': cloudwatch_log_group}
         )
+        print(f"[INFO]: Command executed successfully. Target table ID：[{table_id}]")
+        print(f"[INFO]：Target Network ID：[{network_id}]")
+        return
     except Exception as e:
-        print(f"[ERR]: An error occurred while executing the command. {str(e)}")
+        print(f"[ERR]: An error occurred while executing the command. Target table ID：[{table_id}]")
+        print(f"[INFO]: A-AUTO job startup process terminated abnormally. Target table ID: [{table_id}]")
+        print(f"[INFO]: Target network ID: [{network_id}]")
         return
